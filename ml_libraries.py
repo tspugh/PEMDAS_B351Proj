7bb39598a958f7a87234d8b11422c3c8a99c78a3
Buechler, Trevor E <tbuechle@iu.edu>
2023-03-24 14:47:03 -0400

    Added torch to ML libraries

--- ml_libraries.py
+++ ml_libraries.py
@@ -5,3 +5,4 @@ from sklearn.neural_network import MLPClassifier as mlp
 from sklearn import svm as svm
 from sklearn.model_selection import learning_curve as lc
 import matplotlib.pyplot as plt
+import torch
