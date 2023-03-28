acf9116e763b6d2ec64497047298dbd0fc289b16
Thomas Pugh <tommypugh02@yahoo.com>
2023-03-28 19:30:04 -0400

    DataHarvester - Cleaning Selection
    Chooses certain molecules to work with from a JSON pulled from Pubchem
    Writes the Smiles and names to a txt file

--- data_harvester/pubchem_reader.py
+++ data_harvester/pubchem_reader.py
@@ -16,17 +16,23 @@ PATH = ".."
 #end - what cid to stop at for pubchem
 #file_to_write - optional pickle dump
 def get_molecule_ids(start, end, file_to_write=None):
-    values = empty(end-start+1, dtype="S150")
+    values = []
 
 
     for i in range(end-start+1):
 
         id = i + start
 
-        response = requests.get(f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/{id}/property/InChI/TXT")
+        response = requests.get(
+            f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid"
+            f"/{id}/property/InChI,CanonicalSMILES,Title,"
+            f"HeavyAtomCount/JSON")
         if response:
-            values[i] = response.text[:-1]
+            values.append(response.json()["PropertyTable"][
+                              "Properties"][0])
             print("found "+str(i))
+        else:
+            values.append(None)
     if file_to_write is not None:
         pickle.dump(values, file_to_write, protocol=pickle.HIGHEST_PROTOCOL)
     return values
@@ -50,26 +56,47 @@ def get_spectra(ids, start, end):
     holder = dict()
     if ids is not None:
         search = nist.Search(NoIon=True, cMS=True)
-        for one_id in [x for x in ids if x is not None]:
-            search.find_compounds(identifier=one_id, search_type="inchi")
-            if search.success and len(search.IDs)>0:
-                search.load_found_compounds()
-                compound = search.compounds[0]
-                print(search.compounds[0])
-                compound.get_all_spectra()
-                fuldir = dir+"/"+compound.name+"/"
-                if not os.path.exists(fuldir):
-                    os.makedirs(fuldir)
-                compound.save_all_spectra(path_dir=fuldir)
-                holder.setdefault(one_id, compound)
-    return holder
+        for i in range(len(ids)):
+            one_id = ids[i]["InChI"]
+            if one_id is not None:
+                search.find_compounds(identifier=one_id, search_type="inchi")
+                if search.success and len(search.IDs)>0:
+                    search.load_found_compounds()
+                    compound = search.compounds[0]
+                    print(search.compounds[0])
+                    compound.get_all_spectra()
+                    fuldir = dir+"/"+compound.name+"/"
+                    if not os.path.exists(fuldir):
+                        os.makedirs(fuldir)
+                    compound.save_all_spectra(path_dir=fuldir)
+                    holder.setdefault(one_id, compound)
+                else:
+                    ids[i] = None
 
+    return holder, refine(ids)
 
+def refine(dicts):
+    HEAVY_ATOM_CUTOFF = 10
+    MUST_HAVE_NITROGEN = True
+
+    final = []
+    for i in dicts:
+        if i is not None and int(i["HeavyAtomCount"])<=HEAVY_ATOM_CUTOFF and (not MUST_HAVE_NITROGEN or "N" in i["CanonicalSMILES"]):
+            final.append(i)
+
+    return final
+
+#takes in a list of dictionaries as returned from
+# either get_molecule_ids or refine, and writes in a form suitable to
+# be read later
+def dump_smiles(dictionary, file_to_dump_to):
+    for x in dictionary:
+        file_to_dump_to.write(x["CanonicalSMILES"]+"~"+x["Title"]+"\n")
 
 if __name__ == "__main__":
     #a start and end index of ids to use
-    start = 3001
-    end = 6000
+    start = 1
+    end = 200
 
     #the name of the pickle dump for get_molecule_ids
     id_string = f"tempIds{start},{end}.txt"
@@ -79,7 +106,18 @@ if __name__ == "__main__":
     l = get_molecule_ids(start, end, file_to_write=file_to_write_to)
     file_to_write_to.close()
 
-    #reads from the pickle file and downloads all the spectra
-    #file_to_read_from = open(f"tempIds{start},{end}.txt", "rb")
-    #l = pickle.load(file_to_read_from)
-    #spec = get_spectra(l, start, end)
+    # reads from the pickle file and downloads all the spectra
+    # file_to_read_from = open(f"tempIds{start},{end}.txt", "rb")
+    # l = pickle.load(file_to_read_from)
+
+    #ensures all values meet requirements of our project for now
+    refined_molecules = refine(l)
+
+    spec, refined_molecules = get_spectra(refined_molecules, start, end)
+
+    id_string_smile = f"smiles{start},{end}.txt"
+    f = open(id_string_smile, "w")
+    dump_smiles(refined_molecules, f)
+    f.close()
+
+
