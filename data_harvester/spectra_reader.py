f4ab0d65b74dc81a623cd41155dd8c87f742fdf2
Thomas Pugh <tommypugh02@yahoo.com>
2023-04-05 11:48:50 -0400

    Outlined spectra_reader
    Just needs to be finished

--- data_harvester/spectra_reader.py
+++ data_harvester/spectra_reader.py
@@ -1,39 +1,65 @@
 import os
 import jcamp
+from numpy import zeros
 
 PATH = "../spectra"
 OUTPUT_DIR = "../spectra/csv"
 
-TYPE_OPTIONS = {"MS", "IR", "UV", "TZ"}
+TYPE_OPTIONS = {"MS", "IR", "UV", "1HNMR", "13CNMR"}
 
-#TODO - convert from jdx to csv
-def convert_file(file:str, output_directory:str):
-    pass
+#to be specified
+ARRAY_LENGTH_BY_TYPE = {
+    "MS": 0,
+    "IR": 0,
+    "UV": 0,
+    "1HNMR": 0,
+    "13CNMR": 0
+}
 
-#TODO - read every folder in spectra, look into each of its subfolders, and convert all files of type to csv
+#to be determined
+CLASSIFICATION_TO_VALUE = {
+    "amine": 0,
+    "2-amine": 1,
+    "3-amine": 2,
+    "nitrile": 3,
+    "imine": 4,
+    "amide": 5, #double bond and single bond to Carbons
+    "nitro": 6,
+    "aziridine": 7,
+    "azetidine": 8,
+    "pyrrolidine": 9,
+    "piperidine": 10,
+    "azepane": 11,
+    "azocane": 12
+}
 
-#type can be from TYPE_OPTIONS
-def run_over_all_dirs(type: str):
-    #enter spectra
+class MoleculeObject:
+    def __init__(self, name:str, smiles:str,
+                 classification:str):
+        self.name = name
+        self.smiles = smiles
+        self.classification = classification
+        self.data_arrays = {}
 
-    #for every directory "enumerated" in spectra
+    def set_spectra_to_array(self, file, type):
+        if(type in ["MS", "IR", "UV"]):
+            self.data_arrays.setdefault(type, jdx_to_array(file, type))
+        else:
+            self.data_arrays.setdefault(type, csv_to_array(file,type))
 
-    #for every subdirectory "mol" in "enumerated"
+    #TODO
+    def jdx_to_array(self, file, type):
+        array = list(zeros(ARRAY_LENGTH_BY_TYPE[type]))
 
-    #for each file "jdx_file" in "mol"
+        #use jcamp!
+        # https://pypi.org/project/jcamp/
 
-    #if "jdx_file" has "type" in the name, convert the file
-    pass
+        return None
 
+    #TODO
+    def csv_to_array(self, file, type):
+        array = list(zeros(ARRAY_LENGTH_BY_TYPE[type]))
 
-def get_spectrum(file: str):
-    return Spectrum(file)
+        #tbd
 
-class Spectrum:
-
-    def __init__(self, file):
-        self.parse_file(file)
-
-    # TODO - initialize from the .jdx file
-    def parse_file(self, file):
-        pass
+        return None
